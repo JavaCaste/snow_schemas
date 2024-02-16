fb0a5d87f67444521ada7c872072d25094f9efd2
JavaCaste <138628333+JavaCaste@users.noreply.github.com>
2024-02-16 19:28:11 -0300

    Delete migrations/v.1.1.5__masking_policies.sql

--- migrations/v.1.1.5__masking_policies.sql
+++
@@ -1,7 +0,0 @@
-CREATE MASKING POLICY email_visibility_v2 AS
-(email string, visibility string) RETURNS string ->
-  CASE 
-    WHEN currenr_role()='ACCOUNTADMIN' THEN email
-    WHEN visibility='PUBLIC' THEN email
-    ELSE '******'
-  END;

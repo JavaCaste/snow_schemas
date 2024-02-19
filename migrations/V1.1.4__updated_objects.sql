0f18f34157b1aa3e45d8e83e4837632b796723a8
JavaCaste <138628333+JavaCaste@users.noreply.github.com>
2024-02-19 14:40:47 -0300

    Update V1.1.4__updated_objects.sql

--- migrations/V1.1.4__updated_objects.sql
+++ migrations/V1.1.4__updated_objects.sql
@@ -4,6 +4,7 @@ CREATE MASKING POLICY email_visibility_v3 AS
 (email string, visibility string) RETURNS string ->
   CASE 
     WHEN current_role()='SECURITYADMIN' THEN email
+    WHEN current_role()='ACCOUNTADMIN' THEN email
     WHEN visibility='PUBLIC' THEN email
     ELSE '******'
   END;
